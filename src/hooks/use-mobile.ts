2015137a6c0b0422915af94f47b46cc33642bc8c
GROGAMING <jredmond135@gmail.com>
2026-03-01 22:16:10 +0000

    Improve useIsMobile for older browser support
    Refactor mobile detection for better browser compatibility.

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,9 +1,5 @@
 import * as React from "react";
 
-/**
- * Returns true when the viewport is below the given breakpoint.
- * Default breakpoint matches common mobile width.
- */
 export function useIsMobile(breakpoint = 768): boolean {
   const [isMobile, setIsMobile] = React.useState(false);
 
@@ -13,16 +9,16 @@ export function useIsMobile(breakpoint = 768): boolean {
 
     update();
 
-    // Safari compatibility
     if (typeof media.addEventListener === "function") {
       media.addEventListener("change", update);
       return () => media.removeEventListener("change", update);
-    } else {
-      // @ts-expect-error - older browsers
-      media.addListener(update);
-      // @ts-expect-error - older browsers
-      return () => media.removeListener(update);
     }
+
+    // Older browsers
+    // @ts-ignore
+    media.addListener(update);
+    // @ts-ignore
+    return () => media.removeListener(update);
   }, [breakpoint]);
 
   return isMobile;
